214a40e4dc55c04984418dc0e233ec96f397f616
N3mines <carlson.t.smith@gmail.com>
2022-05-09 20:51:18 -0700

    more comments

--- packages/tensor.py
+++ packages/tensor.py
@@ -4,6 +4,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
+# Class to hold all the data for the model training
 class tensor_data:
     def __init__(self):
         self.session_dir = None
@@ -16,8 +17,11 @@ class tensor_data:
         self.val_ds = None
         self.history = None
 
+    # Build the model outline to train on
     def build_tensor(self, file_name, randomize):
         global tensor
+        
+        # Split up the data into training 80% and validation 20%
         self.train_ds = tf.keras.utils.image_dataset_from_directory(
                 file_name,
                 validation_split=0.2,
@@ -34,6 +38,8 @@ class tensor_data:
                 image_size=(self.img_height, self.img_width),
                 batch_size=self.batch_size)
 
+        # A hack to make randomization work on M1 Macbook Pro silicon.  Without
+        # this tensorflow can't find a device to randomize images
         with tf.device('/CPU:0'):
             data_augmentation = keras.Sequential([
                     layers.RandomFlip("horizontal",
@@ -51,7 +57,9 @@ class tensor_data:
         self.val_ds = self.val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
 
-        # Model shape should always be the same
+        # Model shape should always be the same with randomizationa and without
+
+        # If the user chose to randomize then it is added into the model
         if randomize:
             self.model = Sequential([
                     data_augmentation,
@@ -67,6 +75,8 @@ class tensor_data:
                     layers.Dense(128, activation='relu'),
                     layers.Dense(len(self.class_names))
                 ])
+        # No randomization, normally just going to make the training worse
+        # since the model will trian for characteristics it shouldn't
         else:
             self.model = Sequential([
                     layers.Rescaling(1./255, input_shape=(self.img_height, self.img_width, 3)),
@@ -84,7 +94,8 @@ class tensor_data:
         self.model.compile(optimizer='adam',
                 loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                 metrics=['accuracy'])
-        
+    
+    # Train the model on its data
     def train_tensor(self, epochs):
         global tensor
         self.history = self.model.fit(
@@ -93,6 +104,8 @@ class tensor_data:
             epochs=epochs
         )
     
+    # Run an image through the model and see what it thinks it is.  Only takes
+    # in certain image data types, in this case PIL works
     def predict_tensor(self, img_data):
 
         img_array = tf.keras.utils.img_to_array(img_data)
